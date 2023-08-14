8e35bae8e8a26d2c8802ddcf4070d2194e28c154
Evgeniy Degtyar <110091929+nfadeaway@users.noreply.github.com>
2023-08-14 17:29:42 +0500

    Delete src directory

--- src/__tests__/character.test.js
+++
@@ -1,7 +0,0 @@
-import Character from '../js/Character';
-
-test('Правильно отрабатывает throw при создании экземпляра класса Character', () => {
-  expect(() => {
-    const char = new Character(1);
-  }).toThrow();
-});
